6f467dcabdacab7395f3ddf24671a0084d9cd6b7
Arnaldo Paredes <arnaldop10@gmail.com>
2022-03-27 02:13:09 -0300

    Registrando los modelos para acceder desde el admin de django

--- college/app/admin.py
+++ college/app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Colegio, Alumno, Profesor, Curso, Nota, Prueba
 
-# Register your models here.
+
+admin.site.register(Colegio)
+
+admin.site.register(Alumno)
+
+admin.site.register(Profesor)
+
+admin.site.register(Curso)
+
+admin.site.register(Nota)
+
+admin.site.register(Prueba)
