39fc3e740ba7dcfee6d205e9ee9c4828dc101d62
Arnaldo Paredes <arnaldop10@gmail.com>
2022-03-27 23:17:50 -0300

    Se crean las vistas para crear, editar, leer y eliminar los colegios

--- college/app/urls.py
+++ college/app/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from app.views import index
+from app.views import (
+    ColegioListView,
+    ColegioDetailView,
+    ColegioCreateView,
+    ColegioDeleteView,
+    ColegioUpdateView,
+)
+
+app_name = 'app'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', ColegioListView.as_view(), name='colegio-list'),
+    path('colegio/<int:pk>/detail', ColegioDetailView.as_view(),name='colegio-detail'),
+    path('colegio/create/', ColegioCreateView.as_view(),name='colegio-create'),
+    path('colegio/<int:pk>/update/', ColegioUpdateView.as_view(),name='colegio-update'),
+    path('colegio/<int:pk>/delete/', ColegioDeleteView.as_view(),name='colegio-delete'),
 ]
--- college/app/views.py
+++ college/app/views.py
@@ -1,4 +1,41 @@
+from typing import List
 from django.http import HttpResponse
+from django.views.generic import (
+    DetailView, CreateView, UpdateView, DeleteView, View
+)
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy
+from .models import Colegio, Alumno, Profesor, Curso, Nota, Prueba
 
-def index(request):
-    return HttpResponse('Hola mundo')
+
+############################# COLEGIOS #######################################
+
+class ColegioView(View):
+    model = Colegio
+    fields = '__all__'
+    success_url = reverse_lazy('app:colegio-list')
+
+
+class ColegioListView(ColegioView, ListView):
+    """Listar los colegios registrados"""
+    template_name = "app/colegio/colegio_list.html"
+
+
+class ColegioDetailView(ColegioView, DetailView):
+    """Clase para la vista del detalle del colegio"""
+    template_name = "app/colegio/colegio_detail.html"
+
+
+class ColegioCreateView(ColegioView, CreateView):
+    """Clase para crear un colegio"""
+    template_name = "app/colegio/colegio_form.html"
+
+
+class ColegioUpdateView(ColegioView, UpdateView):
+    """Actualizar los colegios registrados"""
+    template_name = "app/colegio/colegio_form.html"
+
+
+class ColegioDeleteView(ColegioView, DeleteView):
+    """Eliminar los colegios registrados"""
+    template_name = "app/colegio/colegio_confirm_delete.html"
